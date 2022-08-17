ce29176e5467afe8c776ed3cc9333cfcef4601ba
Amjad Majid <amjad@Amjads-MacBook-Pro-2.local>
2022-08-17 12:08:06 +0200

    fix the loading of js code

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,16 +1,16 @@
-function collapsible(){
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
+function collapsible(elm){
+  // var coll = document.getElementsByClassName("collapsible");
+  // var i;
 
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
+  // for (i = 0; i < coll.length; i++) {
+  //   coll[i].addEventListener("click", function() {
+  //     this.classList.toggle("active");
+      var content = elm.nextElementSibling;
       if (content.style.maxHeight){
         content.style.maxHeight = null;
       } else {
         content.style.maxHeight = content.scrollHeight + "px";
       } 
-    });
-  }
+    // });
+  // }
 }
