7d881f456ae563308c7534d3b7788f43bdad4d1d
Amajd Majid <amjadmajid@users.noreply.github.com>
2022-08-17 12:52:20 +0200

    enable the active class via js

--- js/collapsible.js
+++ js/collapsible.js
@@ -4,7 +4,7 @@ function collapsible(elm){
 
   // for (i = 0; i < coll.length; i++) {
   //   coll[i].addEventListener("click", function() {
-  //     this.classList.toggle("active");
+      elm.toggle("active");
       var content = elm.nextElementSibling;
       if (content.style.maxHeight){
         content.style.maxHeight = null;
