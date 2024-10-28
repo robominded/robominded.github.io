0c5b8b882af4a0a5f72411fa88d105c264dae025
Amjad <amjad@AM-MBP.local>
2024-10-28 15:23:44 +0100

    patial update

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,16 +1,16 @@
-function collapsible(elm){
-  // var coll = document.getElementsByClassName("collapsible");
-  // var i;
-
-  // for (i = 0; i < coll.length; i++) {
-  //   coll[i].addEventListener("click", function() {
-      elm.classList.toggle("active");
-      var content = elm.nextElementSibling;
-      if (content.style.maxHeight){
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      } 
-    // });
-  // }
+function collapsible(elm) {
+  elm.classList.toggle("active");
+  var content = elm.nextElementSibling;
+  if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+  } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+  } 
 }
+
+window.onload = function() {
+  var firstContent = document.querySelector('.firstContent');
+  if (firstContent) {
+      firstContent.style.maxHeight = firstContent.scrollHeight + "px";
+  }
+};
